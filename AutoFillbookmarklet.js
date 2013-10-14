d464570923ba1f5dd4b194de6d3ce10534096fc4
Dathann <dathan.n@gmail.com>
2013-10-14 14:46:12 -0700

    Update AutoFillbookmarklet.js

--- AutoFillbookmarklet.js
+++ AutoFillbookmarklet.js
@@ -85,7 +85,7 @@ paste this into a bookmark: javascript: var catchFail = setTimeout(function () {
 
                 if ($('[name*=' + prop + '][type=radio]').length || $('[name*=' + prop + '][type=checkbox]').length) {
                     doCases(prop, function (prop) {
-                        for (var b = 0, a = $('[name*=' + prop + ']') ; b < a.length; b++) {
+                        for (var a = $('[name*=' + prop + ']'), b = a.length ; b > 0; b--) {
                             $('[name*=' + prop + '][value=' + formFillBookmarklet.values[prop] + ']:visible').prop('checked', true)
                         }
                     });
